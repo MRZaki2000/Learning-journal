f2208eab372ad956468c6e5ab0ddbba4ff33de21
MRZaki2000 <57083147+MRZaki2000@users.noreply.github.com>
2022-12-14 18:29:04 +0200

    Add via files upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// const viewEl = document.getElementById("view")
+// const displayNone = document.getElementsByClassName('display-none')
+// console.log(displayNone)
+// console.log(viewEl)
+
+// viewEl.addEventListener("click", function() {
+//     displayNone[0].style.display = "block"
+//     displayNone[1].style.display = "block"
+//     displayNone[2].style.display = "block"
+// })
